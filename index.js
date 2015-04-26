ac628f5245ea465ab3800bf00384c113cff4eb00
Vikrant Varma <vikrant.varma94@gmail.com>
2015-04-26 09:32:36 +0530

    Use Jaro-Winkler distance to detect echoes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+var natural = require('natural');
 
 var server = restify.createServer({
     name: 'chrome-stt-server'
@@ -10,6 +11,17 @@ server.use(restify.queryParser());
 
 var xal = require('../../xal-javascript');
 
+var lastSeenOutput = null;
+var lastSeenOutputTime = new Date();
+
+xal.on('xi.event.output.text', function(state, next) {
+    var output = state.get('xi.event.output.text')[0].value;
+    output = output.replace(/[^A-Za-z0-9 ]/, '');
+    output = output.toLowerCase();
+    lastSeenOutput = output;
+    lastSeenOutputTime = new Date();
+});
+
 server.get('/', function(req, res, next) {
     var message = req.query.stt;
     res.send("STT Okay");
@@ -18,7 +30,13 @@ server.get('/', function(req, res, next) {
     message = message.replace(/\bbm\b/ig, 'pm');
     message = message.replace(/\b(\d\d)(\d\d) (am|pm)\b/, "$1:$2 $3");
     message = message.trim();
-    xal.log.info("Received STT:", message);
+    xal.log.debug("Received STT:", message);
+    var distance = natural.JaroWinklerDistance(message, lastSeenOutput);
+    xal.log.debug({JWD: distance});
+    if (distance > 0.8) {
+        xal.log.debug('Output echo suspected');
+        return next();
+    }
     xal.createEvent('xi.event.input.text', function(state, done) {
         state.put('xi.event.input.text', message);
         done(state);
